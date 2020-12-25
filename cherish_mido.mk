ee2062a4d5c87127b301a44e30f7679cee786556
Hưng Phan <phandinhhungvp2001@gmail.com>
2020-12-25 22:01:35 +0700

    cherish
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- cherish_mido.mk
+++ cherish_mido.mk
@@ -22,11 +22,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
+CHERISH_BUILD_TYPE :=OFFICIAL
+CHERISH_WITHGAPPS=true
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := cherish_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
